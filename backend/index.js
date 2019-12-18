9a0ba834ee21013cd6f98583a7defd18f0e17b0d
César Polanco <cpolcam@outlook.com>
2019-12-17 20:32:36 -0600

    primer commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+//Modulo para visualizarp por consola peticiones http
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+//Initializations
+const app = express();
+
+//Settings
+app.set('port', 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,'public/uploads'),
+    filename(req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+})
+
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+//routes
+app.use('/api/books',require('./routes/books'));
+
+
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Start a server
+app.listen(app.get('port'), () => {
+    console.log('Server on port: ', app.get('port'));
+})
