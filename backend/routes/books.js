57b3832714d16df88bdac1b6193c15836a45946c
César Polanco <cpolcam@gmail.com>
2019-12-26 10:56:51 -0600

    API finalizada
    Se agregaron las rutas Post y Delete al api.

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,9 +2,24 @@ const {Router} = require('express');
 const router = Router();
 const Book = require('../models/book');
 
+//GET
 router.get('/', async (req, res) => {
     const books = await Book.find();
     res.json(books);
 });
 
+//POST
+router.post('/', async (req, res) =>{
+    const {title, author, isbn} = req.body;
+    const newBook =  new Book({title, author, isbn});
+    await newBook.save();
+    res.json({message: 'Book saved'});
+})
+
+//DELETE
+router.delete('/:id', async (req, res) =>{
+    res.send('deleting');
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({message: 'Book deleted'});
+});
 module.exports = router;
